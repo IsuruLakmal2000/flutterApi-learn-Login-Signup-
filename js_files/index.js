f22f2acfee3c8a357882e40c5d22e48b601796b0
isuru <isurulakmalid13@gmail.com>
2023-01-22 21:18:42 +0530

    PORT

--- js_files/index.js
+++ js_files/index.js
@@ -8,7 +8,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/myapp",{
 
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 const connection = mongoose.connection;
