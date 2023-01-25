be867c88d29e037a82fabca4ce13ebceb0bed985
isuru <isurulakmalid13@gmail.com>
2023-01-25 21:54:32 +0530

    PjhasdsdsaaT

--- lib/signup.dart
+++ lib/signup.dart
@@ -318,7 +318,7 @@ class _SignUpPageState extends State<SignUpPage> {
   checkUser() async {
     var response =
         await networkHandler.get("user/checkusername/${_controller.text}");
-    if (response['status']) {
+    if (response['Status']) {
       setState(() {
         isUnique = true;
       });
